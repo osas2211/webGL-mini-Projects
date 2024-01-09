32bb928569705431b6f1dd3511c1f84762ac391b
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-09 06:55:57 +0100

    add fullscreen and responsiveness

--- orbit-box/dist/main.js
+++ orbit-box/dist/main.js
@@ -1,9 +1,26 @@
 import * as THREE from "three";
-const sizes = {
+let sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
-const aspect_ratio = sizes.width / sizes.height;
+let aspect_ratio = sizes.width / sizes.height;
+window.addEventListener("resize", () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+window.addEventListener("dblclick", () => {
+    const fullScreen = document.fullscreenElement;
+    if (fullScreen) {
+        document.exitFullscreen();
+    }
+    else {
+        canvas.requestFullscreen();
+    }
+});
 const canvas = document.querySelector(".webGL");
 const scene = new THREE.Scene();
 const group = new THREE.Group();
@@ -15,6 +32,7 @@ scene.add(group);
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspect_ratio);
 camera.position.z = 3;
+scene.add(camera);
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
--- orbit-box/src/main.ts
+++ orbit-box/src/main.ts
@@ -1,10 +1,29 @@
 import * as THREE from "three"
 
-const sizes = {
+let sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
-const aspect_ratio = sizes.width / sizes.height
+let aspect_ratio = sizes.width / sizes.height
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener("dblclick", () => {
+  const fullScreen = document.fullscreenElement
+  if (fullScreen) {
+    document.exitFullscreen()
+  } else {
+    canvas.requestFullscreen()
+  }
+})
 
 const canvas = document.querySelector(".webGL") as HTMLCanvasElement
 
@@ -20,7 +39,9 @@ scene.add(group)
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspect_ratio)
 camera.position.z = 3
+scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
+
 renderer.render(scene, camera)
