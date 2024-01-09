3476f1b902366c745cff41831eab321431986807
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-09 07:06:26 +0100

    add some comments

--- orbit-box/dist/main.js
+++ orbit-box/dist/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// Define Sizes and Responsiveness
 let sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
@@ -23,6 +24,7 @@ window.addEventListener("dblclick", () => {
     }
 });
 const canvas = document.querySelector(".webGL");
+// Add Scene
 const scene = new THREE.Scene();
 const group = new THREE.Group();
 const box = new THREE.BoxGeometry(1, 1, 1);
@@ -30,14 +32,17 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(box, material);
 group.add(mesh);
 scene.add(group);
-// Camera
+// Add Camera
 const camera = new THREE.PerspectiveCamera(75, aspect_ratio);
 camera.position.z = 3;
 scene.add(camera);
+// Add Orbit Controls
 const orbitConrols = new OrbitControls(camera, canvas);
 orbitConrols.enableDamping = true;
+// Add Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+// Add Animation Function
 const animate = () => {
     window.requestAnimationFrame(animate);
     orbitConrols.update();
--- orbit-box/src/main.ts
+++ orbit-box/src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
+// Define Sizes and Responsiveness
 let sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -28,6 +29,8 @@ window.addEventListener("dblclick", () => {
 
 const canvas = document.querySelector(".webGL") as HTMLCanvasElement
 
+// Add Scene
+
 const scene = new THREE.Scene()
 const group = new THREE.Group()
 const box = new THREE.BoxGeometry(1, 1, 1)
@@ -37,17 +40,20 @@ group.add(mesh)
 
 scene.add(group)
 
-// Camera
+// Add Camera
 const camera = new THREE.PerspectiveCamera(75, aspect_ratio)
 camera.position.z = 3
 scene.add(camera)
 
+// Add Orbit Controls
 const orbitConrols = new OrbitControls(camera, canvas)
 orbitConrols.enableDamping = true
 
+// Add Renderer
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 
+// Add Animation Function
 const animate = () => {
   window.requestAnimationFrame(animate)
   orbitConrols.update()
