d26d2abd12d057f7453a54fd51d425893a25e03c
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-09 06:22:57 +0100

    add camera

--- orbit-box/dist/main.js
+++ orbit-box/dist/main.js
@@ -1,4 +1,9 @@
 import * as THREE from "three";
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+const aspect_ratio = sizes.width / sizes.height;
 const scene = new THREE.Scene();
 const group = new THREE.Group();
 const box = new THREE.BoxGeometry(1, 1, 1);
@@ -6,3 +11,5 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(box, material);
 group.add(mesh);
 scene.add(group);
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspect_ratio);
--- orbit-box/src/main.ts
+++ orbit-box/src/main.ts
@@ -1,5 +1,12 @@
 import * as THREE from "three"
 
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const aspect_ratio = sizes.width / sizes.height
+
 const scene = new THREE.Scene()
 const group = new THREE.Group()
 const box = new THREE.BoxGeometry(1, 1, 1)
@@ -8,3 +15,6 @@ const mesh = new THREE.Mesh(box, material)
 group.add(mesh)
 
 scene.add(group)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, aspect_ratio)
