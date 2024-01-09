131e818d552b95a094f83a3cc56db9b7b21370bc
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-09 06:19:06 +0100

    add MeshGroup to scene

---
+++ orbit-box/src/main.ts
@@ -0,0 +1,10 @@
+import * as THREE from "three"
+
+const scene = new THREE.Scene()
+const group = new THREE.Group()
+const box = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(box, material)
+group.add(mesh)
+
+scene.add(group)
